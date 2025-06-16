bee7130637dda28c631a5442cf26f6d25d14a5b4
Priyanshu0512 <priyanshuk0595@gmail.com>
2025-06-16 16:35:06 +0530

    Initial Setup with Clerk and database intialization.

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div className="w-full flex min-h-screen items-center justify-center">
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/app/layout.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
+
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+export const metadata: Metadata = {
+  title: "Vivid-AI",
+  description: "Build A.I. powered presentation.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
